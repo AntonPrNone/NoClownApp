a0b2bf329cd9bff0a35a8a17522e1d7a255e704c
Anton <yznikfortyni@gmail.com>
2023-03-05 23:29:46 +0300

    ЗаданиеСделаноМожноОткатыватьсяЕслиОпятьХерняБудет

--- flutter_application_1/lib/WidgetDealPage.dart
+++ flutter_application_1/lib/WidgetDealPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'secondScreen.dart';
 
@@ -12,29 +13,91 @@ class DealPage extends StatefulWidget {
 }
 
 class _DealPageState extends State<DealPage> {
+  void DeleteAdd(String? T, String? ST) async {
+    if (T == null) {
+      users.remove("");
+    } else {
+      users.remove(T);
+    }
+
+    if (ST == null) {
+      companies.remove("");
+    } else {
+      companies.remove(ST);
+    }
+
+    await _setData();
+  }
+
+  Future _setData() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList("UsersList", users);
+    prefs.setStringList("CompaniesList", companies);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: users.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-                border: Border.all(width: 2),
-                color: Color.fromARGB(255, 201, 201, 201),
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            child: ListTile(
-                title: Text(users[index], style: TextStyle(fontSize: 20)),
-                leading: Text(
-                  (index + 1).toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                trailing: Icon(Icons.delete_forever),
-                subtitle: Text(
-                  companies[index],
-                )),
-          );
-        });
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: users.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.red),
+                            color: Color.fromARGB(255, 108, 75, 255),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        padding: EdgeInsets.all(8),
+                        child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Center(
+                              child: Text(users[index],
+                                  style: TextStyle(
+                                      fontSize: 22, color: Colors.white)),
+                            ))),
+                  );
+                }),
+          ),
+        ),
+        Expanded(
+          flex: 8,
+          child: SizedBox(
+              child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: users.length,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 2),
+                          color: Color.fromARGB(255, 201, 201, 201),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: ListTile(
+                          title: Text(users[index],
+                              style: TextStyle(fontSize: 20)),
+                          leading: Text(
+                            (index + 1).toString(),
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete_forever),
+                            onPressed: () {},
+                          ),
+                          subtitle: Text(
+                            companies[index],
+                          )),
+                    );
+                  })),
+        )
+      ],
+    );
   }
 }
